14b9da30030beb2d6e0ef9e8a8f79f91189a452c
kimguan-breeze <chukimguan@breeze.cash>
2025-10-17 12:07:13 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import "@breeze.cash/ui/styles.css";
-
 function App() {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    fetch("https://da9daf1d68a7.ngrok-free.app/v1/status", {
-      credentials: "include", // This is correct for sending cookies
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log({ data });
-        setSession(data.data.session);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        // Handle error state appropriately
-      });
-  }, []);
-
-  return <>{session}</>;
+  return (
+    <iframe src="https://link.qa.breeze.cash/link/plink_6863c667a3f4f6ae"></iframe>
+  );
 }
 
 export default App;
