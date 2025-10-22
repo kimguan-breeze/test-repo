a6dd237536767d039867a9033259eb90d1582500
kimguan-breeze <chukimguan@breeze.cash>
2025-10-22 11:13:16 +0800

    feat: add post message

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,32 @@
+import { useCallback, useEffect } from "react";
+
 function App() {
+  const handleIFrameRequest = useCallback(async (event: MessageEvent) => {
+    if (event.data.type === "request-global-config") {
+      const iframe = document.querySelector("iframe");
+      if (iframe && iframe.contentWindow) {
+        iframe.contentWindow.postMessage(
+          {
+            type: "request-global-config",
+            config: {
+              applePayEnabled: true,
+              crossDomainName: location.host,
+            },
+          },
+          "*"
+        );
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("message", handleIFrameRequest);
+
+    return () => {
+      window.removeEventListener("message", handleIFrameRequest);
+    };
+  }, [handleIFrameRequest]);
+
   return (
     <iframe
       style={{ width: "100%", height: "100vh" }}
