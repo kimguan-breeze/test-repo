9116203c3998f3178e098fff06f0969665e7c95e
kimguan-breeze <chukimguan@breeze.cash>
2025-10-22 10:21:26 +0800

    update link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <iframe src="https://link.qa.breeze.cash/link/plink_6863c667a3f4f6ae"></iframe>
+    <iframe src="https://link.qa.breeze.cash/link/plink_80126366fcbaa311"></iframe>
   );
 }
 
