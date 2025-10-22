2221b214c8c54375712c61a2d110fb440dede85a
kimguan-breeze <chukimguan@breeze.cash>
2025-10-22 10:49:58 +0800

    change url

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <iframe
       style={{ width: "100%", height: "100vh" }}
       src="https://pay.qa.breeze.cash/page_fb50a76c6f64f62f/pcs_e670227fee25bef86462d11e58f86437aa80882a/card?livemode=false"
-      allow="payment https://pay.breeze.cash"
+      allow="payment https://pay.qa.breeze.cash"
     ></iframe>
   );
 }
