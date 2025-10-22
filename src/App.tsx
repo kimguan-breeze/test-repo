638997014c5bf2e02b903efbd3f83ee426970373
kimguan-breeze <chukimguan@breeze.cash>
2025-10-22 10:48:16 +0800

    pay domain

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <iframe
       style={{ width: "100%", height: "100vh" }}
       src="https://link.qa.breeze.cash/link/plink_80126366fcbaa311"
-      allow="payment"
+      allow="payment https://pay.breeze.cash"
     ></iframe>
   );
 }
